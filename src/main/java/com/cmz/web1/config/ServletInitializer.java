1fc7e5b7ff09573916ab544c07b983cca78ac936
chmzh <28458942@qq.com>
2016-11-23 08:38:33 +0800

    Add files via upload

--- src/main/java/com/cmz/web1/config/ServletInitializer.java
+++ src/main/java/com/cmz/web1/config/ServletInitializer.java
@@ -1,15 +1,19 @@
 package com.cmz.web1.config;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.FilterRegistration.Dynamic;
 
 import org.springframework.util.ClassUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
 public class ServletInitializer extends AbstractDispatcherServletInitializer {
 
+	
 	@Override
 	protected WebApplicationContext createServletApplicationContext() {
 		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
@@ -28,9 +32,32 @@ public class ServletInitializer extends AbstractDispatcherServletInitializer {
 	}
 	
 	@Override
+	protected Dynamic registerServletFilter(ServletContext servletContext, Filter filter) {
+		// TODO Auto-generated method stub
+		
+		return super.registerServletFilter(servletContext, filter);
+	}
+	
+//	@Override
+//	protected Filter[] getServletFilters() {
+//		Filter[] filters = new Filter[1];
+//		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+//		characterEncodingFilter.setEncoding("UTF-8");
+//		characterEncodingFilter.setForceEncoding(true);
+//		filters[0] = characterEncodingFilter;
+//		
+//		return filters;
+//	}
+	
+	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
+		
 		super.onStartup(servletContext);
-//		DelegatingFilterProxy filter = new DelegatingFilterProxy("springSecurityFilterChain");
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		servletContext.addFilter("characterEncodingFilter", characterEncodingFilter).addMappingForUrlPatterns(null, false, "/*");
+		//		DelegatingFilterProxy filter = new DelegatingFilterProxy("springSecurityFilterChain");
 //		filter.setContextAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.dispatcher");
 //		servletContext.addFilter("springSecurityFilterChain", filter).addMappingForUrlPatterns(null, false, "/*");
 	}
--- src/main/java/com/cmz/web1/config/WebMvcConfig.java
+++ src/main/java/com/cmz/web1/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.cmz.web1.config;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +19,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
@@ -31,20 +37,35 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
+	public FreeMarkerConfigurer freemarkerConfig(){
+		FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+		configurer.setTemplateLoaderPath("/WEB-INF/views/");
+		configurer.setDefaultEncoding("UTF-8");
+		return configurer;
+	}
+	@Bean
 	public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
 		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
 		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
 
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/jsp/");
-		viewResolver.setSuffix(".jsp");
-
+		//InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		//viewResolver.setPrefix("/WEB-INF/jsp/");
+		//viewResolver.setSuffix(".jsp");
+		UrlBasedViewResolver urlBasedViewResolver = new UrlBasedViewResolver();
+		//urlBasedViewResolver.setPrefix("/WEB-INF/views/");
+		urlBasedViewResolver.setSuffix(".html");
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put("input.encoding", "UTF-8");
+		attributes.put("output.encoding", "UTF-8");
+		urlBasedViewResolver.setAttributesMap(attributes);
+		urlBasedViewResolver.setViewClass(FreeMarkerView.class);
+		
 		MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
 		defaultView.setExtractValueFromSingleKeyModel(true);
-
+	
 		ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
 		contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
-		contentViewResolver.setViewResolvers(Arrays.<ViewResolver> asList(viewResolver));
+		contentViewResolver.setViewResolvers(Arrays.<ViewResolver> asList(urlBasedViewResolver));
 		contentViewResolver.setDefaultViews(Arrays.<View> asList(defaultView));
 		return contentViewResolver;
 	}
